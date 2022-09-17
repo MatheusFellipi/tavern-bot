09599374a62d4688e2923c039d3be6ec156fb3ab
Matheus Fellipi <matheus.fellipi@outlook.com>
2022-09-17 13:46:13 -0300

    feat:add routes to add commands in discord

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,3 +1,5 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const fs = require('fs');
 
 module.exports = (client) => {
@@ -9,12 +11,29 @@ module.exports = (client) => {
         .filter((file) => file.endsWith('.js'));
 
       const { commands, commandArray } = client;
+
       for (const file of commandFiles) {
         const command = require(`../../commands/${folder}/${file}`);
         commands.set(command.data.name, command);
-        commandArray.push(command, command.data.toJson());
-        console.log(`Command: ${command.data.name} has been registered`)
+        commandArray.push(command.data.toJSON());
+        console.log(`Command: ${command.data.name} has been registered`);
       }
+    }
+    const CLIENT_ID = '1020487724777615430';
+    const GUILD_ID = '1020298551642837023';
+
+    const rest = new REST({ version: '9' }).setToken(
+      process.env.DISCORD_BOT_TOKEN
+    );
+
+    try {
+      console.log('started refreshing application (/) commands');
+      await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+        body: client.commandArray,
+      });
+      console.log('Successfully reloaded application (/) commands');
+    } catch (error) {
+      console.error(error);
     }
   };
 };
